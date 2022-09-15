de8c70cba7ef9be6fc4f6d41599e84e99c1cf0d2
Grb0018 <105971445+Grb0018@users.noreply.github.com>
2022-09-15 12:49:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -85,6 +85,7 @@ if(navigator.userAgent.includes('Mobile')){
         alert('potrait')
         $('#short').css({display: 'flex'});
         var sr = setInterval(()=>{
+        alert(parseInt(screen.availWidth))    
             if(parseInt(screen.availWidth)>480){
                 alert('landscape')
                 window.location.href='https://grb0018.github.io/game1';
